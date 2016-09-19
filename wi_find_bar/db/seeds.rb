cd41f95739372733339c14a58ae96e272f4707e3
Ezequiel Zimenspitz <ezeqzim@gmail.com>
2016-09-19 14:44:06 -0300

    Update seed.rb for demo

--- wi_find_bar/db/seeds.rb
+++ wi_find_bar/db/seeds.rb
@@ -7,17 +7,17 @@ bar6 = FactoryGirl.create(:bar, name: 'Raw', wifi: false)
 bar7 = FactoryGirl.create(:bar, name: 'Antares', wifi: true)
 bar8 = FactoryGirl.create(:bar, name: 'Sugar Bs As', wifi: true)
 
-FactoryGirl.create(:location, lat: -34.5455969, lng: -58.4402055,
+FactoryGirl.create(:location, lat: -34.5455925, lng: -58.4402055,
                               address: 'Intendente Güiraldes 22', bar: bar1)
-FactoryGirl.create(:location, lat: -34.5461594, lng: -58.4407214,
+FactoryGirl.create(:location, lat: -34.546155, lng: -58.4407214,
                               address: 'Intendente Güiraldes 24', bar: bar2)
 FactoryGirl.create(:location, lat: -34.556668, lng: -58.4498764,
                               address: 'Juramento 1499', bar: bar3)
-FactoryGirl.create(:location, lat: -34.546155, lng: -58.4407214,
-                              address: 'Intendente Güiraldes 20', bar: bar4)
-FactoryGirl.create(:location, lat: -34.5815033, lng: -58.4259446,
+FactoryGirl.create(:location, lat: -34.5452241, lng: -58.4410597,
+                              address: 'Intendente Güiraldes 200', bar: bar4)
+FactoryGirl.create(:location, lat: -34.5814989, lng: -58.4259446,
                               address: 'Güemes 4600', bar: bar5)
-FactoryGirl.create(:location, lat: -34.5805608, lng: -58.4275314,
+FactoryGirl.create(:location, lat: -34.5805564, lng: -58.4275314,
                               address: 'Güemes 4800', bar: bar6)
 FactoryGirl.create(:location, lat: -34.5574032, lng: -58.451084,
                               address: 'Juramento 1599', bar: bar7)
